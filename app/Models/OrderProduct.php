29f7ef339db51ce15cb26064e525af8584ae1e13
Anwar-Barakat <brkatanwar0@gmail.com>
2023-06-16 14:36:11 +0300

    adding order products table

---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderProduct extends Model
+{
+    use HasFactory;
+
+    protected $fillable =[
+        'user_id',
+        'order_id',
+        'product_id',
+        'qty',
+        'product_size',
+        'product_price',
+        'grand_price',
+    ];
+}
