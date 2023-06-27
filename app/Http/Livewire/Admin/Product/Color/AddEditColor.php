98aa203b7820c75698dfe852fdb71348293c902c
Anwar-Barakat <brkatanwar0@gmail.com>
2023-06-27 13:54:09 +0300

    show product colors on the product details page within the admin dashboard

--- app/Http/Livewire/Admin/Product/Color/AddEditColor.php
+++ app/Http/Livewire/Admin/Product/Color/AddEditColor.php
@@ -7,9 +7,12 @@ use App\Models\Product;
 use App\Models\ProductColor;
 use Illuminate\Validation\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AddEditColor extends Component
 {
+    use WithPagination;
+
     public Product $product;
 
     public $color,
@@ -24,7 +27,8 @@ class AddEditColor extends Component
 
     public function updateStatus(ProductColor $color)
     {
-        $color->update(['is_active' => !$color->is_active]);
+        $color->is_active = !$color->is_active;
+        $color->save();
     }
 
     public function submit()
@@ -40,7 +44,7 @@ class AddEditColor extends Component
         }
     }
 
-    public function edit(ProductColor $color)
+    public function update(ProductColor $color)
     {
         $this->color = $color;
     }
@@ -53,7 +57,9 @@ class AddEditColor extends Component
 
     public function render()
     {
-        return view('livewire.admin.product.color.add-edit-color', ['productColors' => $this->getProductColors()]);
+        return view('livewire.admin.product.color.add-edit-color', [
+            'productColors'     => $this->getProductColors()
+        ]);
     }
 
     public function rules()
